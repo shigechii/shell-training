36ca737d1fdfa882a2c8ac2217a177e149f54867
shigechii <ayumu.shigeta.tech@gmail.com>
2025-04-21 23:16:25 +0900

    標準出力をファイルに追記する

--- src/std/std.sh
+++ src/std/std.sh
@@ -6,9 +6,14 @@ DATA_FILE_PATH="$(cd "$(dirname "$0")/data" && pwd)/data_file"
 # ファイルが存在しない場合、ファイルが作成される
 echo "aaa" > $DATA_FILE_PATH
 
+sleep 3
+
 rm $DATA_FILE_PATH
 
 sleep 3
 
 # echo "bbb" > $DATA_FILE_PATH と同じ結果
 echo "bbb" 1> $DATA_FILE_PATH
+
+# >> ファイル末部に追記する
+echo "ccc" >> $DATA_FILE_PATH
